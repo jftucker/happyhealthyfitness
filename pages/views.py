35f82bcbad405607a49de492704666acbab0c6f0
jftucker <johntucker0686@gmail.com>
2019-08-11 09:28:49 -0700

    updating content, email flow for new users

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import ListView
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 
 from articles.models import Article
@@ -10,6 +10,9 @@ from .forms import ContactForm
 from django.core.mail import EmailMessage, BadHeaderError, EmailMultiAlternatives
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from allauth.account.models import EmailAddress
+from allauth.account.utils import send_email_confirmation
 
 
 class HomePageView(ListView):
@@ -51,7 +54,16 @@ class HomePageView(ListView):
             new_user = get_user_model().objects.create_user(
                 username=form.cleaned_data['email'],
                 email=form.cleaned_data['email'],
-                from_frontpage=True,
-                on_newsletter=form.cleaned_data['mailer'],   
-            )   
+            )
+            new_user.from_frontpage=True
+            new_user.on_newsletter=form.cleaned_data['mailer']
+            new_user.save()
+            
+            EmailAddress.objects.create(user=new_user, email=new_user.email, primary=True, verified=False)
+            send_email_confirmation(request, new_user)
+
             return redirect('home')
+
+
+#class EbookPageView(TemplateView):
+#    template = 'download.html'
