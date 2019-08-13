5e9b8c28d590ed504a4197df6b444d61a155e0a0
jftucker <johntucker0686@gmail.com>
2019-08-12 21:35:30 -0700

    add image model to admin for articles

--- articles/admin.py
+++ articles/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django import forms
 
-from .models import Article, Comment
+from .models import Article, Comment, Image
 
 from ckeditor.widgets import CKEditorWidget
 
 
 class CommentInline(admin.TabularInline):
     model = Comment
+    extra = 0
+
+class ImageInline(admin.TabularInline):
+    model = Image
     extra = 0
 
 class ArticleAdminForm(forms.ModelForm):
@@ -21,6 +25,7 @@ class ArticleAdmin(admin.ModelAdmin):
     
     form = ArticleAdminForm
     inlines = [
+        ImageInline,
         CommentInline,
     ]
 
